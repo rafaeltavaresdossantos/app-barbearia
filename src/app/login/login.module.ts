1330a8aff2ee9bd450aad3b5ba3b3790f2f685a7
rafael <rafael-tavares@live.com>
2020-01-18 15:18:15 -0300

    organizando core module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
-import { Route } from '@angular/compiler/src/core';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
 import { SharedModule } from '../shared/shared.module';
 
 const rotas: Routes = [{
