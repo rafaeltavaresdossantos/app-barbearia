0474451045bf11af8db2679208a76f4bca964f4e
rafael <rafael-tavares@live.com>
2020-01-18 16:05:47 -0300

    implementando html login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+
+    this.criaFormulario();
+  }
+
+  // loginform recebe FormBuilder e realiza a validação do campo.
+  criaFormulario(){
+    this.loginForm = this.fb.group({
+      email: ['',[
+        Validators.required,
+        Validators.email
+        ]
+      ],
+      senha: [ '',[
+        Validators.required,
+        Validators.minLength[6],
+      ]
+    ],
+      
+    })
+  }
+
+  realizarLogin(){
+    console.log("entrou",this.loginForm.value);
+  }
 
 }
