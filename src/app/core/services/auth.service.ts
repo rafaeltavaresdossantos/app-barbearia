1b972d4ee50a0cb7fb1eb13dd2c065b51e943973
rafael <rafael-tavares@live.com>
2020-01-25 14:49:00 -0300

    criada funcao cadastrar com email

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -15,6 +15,18 @@ export class AuthService {
   loginComEmail({email,senha}) :Promise<auth.UserCredential>{
     return this.afauth.auth.signInWithEmailAndPassword(email,senha);
   }
+
+  //funcao responsavel por cadastar o usuario no firebase, se o retorno for bem sucedido autoriza a inclusão do nome
+  cadastrarComEmail({email,senha,nome}) :Promise<auth.UserCredential>{
+    return this.afauth.auth.createUserWithEmailAndPassword(email,senha)
+      .then(res => {
+        return res.user.updateProfile({
+          displayName: nome,
+          photoURL: null,
+        }).then( () => res )
+      })
+
+  }
 }
 
  
