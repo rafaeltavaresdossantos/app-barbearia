01b7ebe12485f9007e5d7cc8349dd265a1ad649c
rafael <rafael-tavares@live.com>
2020-02-01 14:51:17 -0300

    implementando o auth guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { tap, take } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+
+  constructor(
+
+    private authService: AuthService,
+    private router: Router
+
+  ){}
+
+  private verificaEstadoUsuario(url :string) :Observable<boolean>{
+    return this.authService.isAutenticado.pipe(
+      tap(res => {
+        if(!res){
+          this.router.navigate(['/login'], {
+            queryParams: {url}
+          })
+        }
+      })
+    )
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+    return this.verificaEstadoUsuario(state.url);
+  }
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+    return this.canActivate(route, state);
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> {
+      const url = segments.map(s => `/${s}`).join('')
+    return this.verificaEstadoUsuario(url).pipe(
+      take(1)
+    );
+  }
+}
