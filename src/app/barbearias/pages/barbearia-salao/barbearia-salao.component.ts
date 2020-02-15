e12c4da01f268e161f8cb3b06076b71a97103ead
rafael <rafael-tavares@live.com>
2020-02-15 16:30:28 -0300

    implementacao da tela barbeiros estilização e consumindo os dados do banco

--- src/app/barbearias/pages/barbearia-salao/barbearia-salao.component.ts
+++ src/app/barbearias/pages/barbearia-salao/barbearia-salao.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { BarbeariasService } from '../../services/barbearias.service';
 import { Observable } from 'rxjs';
 import { Barbearia } from '../../models/barbearia.model';
+import { BarbeiroService } from 'src/app/barbeiro/services/barbeiro.service';
+import { Barbeiro } from 'src/app/barbeiro/models/barbeiro.model';
+import { IMAGEM_BARBEIRO_PADRAO } from 'src/app/barbeiro/constantes/imagem-barbeiro-padrao';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-barbearia-salao',
@@ -17,33 +21,27 @@ export class BarbeariaSalaoComponent implements OnInit {
     centeredSlides: true
   };
 
-  public barbeiros = [
-    {
-      nome: 'Jeferson',
-      exp: 'Cortes desenhados'
-    },
-    {
-    nome: 'João',
-    exp: 'Degradê'
-    },
-    {
-      nome: 'Gabriel',
-      exp: ` - Barba
-             - Cortes: Simples, Degradê, Frizado e Desenho.  
-        `
-    }
-  ]
+  public barbeiros$ : Observable<Barbeiro[]> 
   public barbearia$ :Observable<Barbearia>
+  public imagemPadrao = IMAGEM_BARBEIRO_PADRAO;
 
   constructor(
     private route: ActivatedRoute,
-    private barbeariasService: BarbeariasService
+    private barbeariasService: BarbeariasService,
+    private barbeiroService: BarbeiroService,
+    private navCtrl: NavController
   ) {}
 
   ngOnInit() {
     //Pegando rota do navegador, apenas id
     const id = this.route.snapshot.paramMap.get('id')
     this.barbearia$ = this.barbeariasService.get(id)
+    this.barbeiros$ = this.barbeiroService.listarBarbeirosPorBarbearia(id);
   }
+  acessarBarbeiro(id: string){
+    this.navCtrl.navigateForward(['barbeiro', id]);
+  }
+  
+
 
 }
