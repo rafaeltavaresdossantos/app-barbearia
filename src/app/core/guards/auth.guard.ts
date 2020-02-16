7d62c269fb935ca9d0965ce580dff9c1f6ffcb65
Andrew Rosário <andrew.arosario@gmail.com>
2020-02-15 21:20:13 -0300

    refatoração do auth.guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  CanActivate,
+  CanActivateChild,
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { tap, take } from 'rxjs/operators';
@@ -10,39 +19,36 @@ import { tap, take } from 'rxjs/operators';
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
 
   constructor(
-
     private authService: AuthService,
     private router: Router
+  ) {}
 
-  ){}
-
-  private verificaEstadoUsuario(url :string) :Observable<boolean>{
+  private verificaEstadoUsuario(url: string): Observable<boolean> {
     return this.authService.isAutenticado.pipe(
       tap(res => {
-        if(!res){
+        if (!res) {
           this.router.navigate(['/login'], {
             queryParams: {url}
-          })
+          });
         }
       })
-    )
+    );
   }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> {
     return this.verificaEstadoUsuario(state.url);
   }
+
   canActivateChild(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> {
     return this.canActivate(route, state);
   }
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> {
-      const url = segments.map(s => `/${s}`).join('')
-    return this.verificaEstadoUsuario(url).pipe(
-      take(1)
-    );
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> {
+    const url = segments.map(s => `/${s}`).join('');
+    return this.verificaEstadoUsuario(url).pipe(take(1));
   }
 }
