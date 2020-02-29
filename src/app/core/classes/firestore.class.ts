32552252e3f15eab75b615984ddd7c5ac17a225f
Andrew Rosário <andrew.arosario@gmail.com>
2020-02-29 16:41:54 -0300

    incluindo métodos padrão para manipulação de documents

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -1,13 +1,18 @@
-import { AngularFirestore, AngularFirestoreCollection, QueryFn } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, QueryFn, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 export abstract class Firestore<T extends { id: string }> {
   protected collection: AngularFirestoreCollection<T>;
+  protected doc: AngularFirestoreDocument<T>;
 
   constructor(protected db: AngularFirestore) {}
 
   protected setCollection(path: string, queryFn?: QueryFn): void {
     this.collection = path ? this.db.collection(path, queryFn) : null;
+  }
+
+  protected setDoc(path: string): void {
+    this.doc = path ? this.db.doc(path) : null;
   }
 
   private setItem(item: T, operation: string): Promise<T> {
@@ -36,5 +41,16 @@ export abstract class Firestore<T extends { id: string }> {
 
   delete(item: T): Promise<void> {
     return this.collection.doc<T>(item.id).delete();
+  }
+
+  createDoc(item: T) {
+    return this.doc.set(item).then(() => item);
+  }
+
+  deleteDoc(): Promise<void> {
+    if (!this.doc) {
+      return ;
+    }
+    return this.doc.delete();
   }
 }
