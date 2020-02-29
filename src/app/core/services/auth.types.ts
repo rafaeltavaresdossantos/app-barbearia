7ab6d5e062e2d80f93f88f75a23f6e8f4b19d5bd
Andrew Rosário <andrew.arosario@gmail.com>
2020-02-29 16:40:46 -0300

    implementando tipos de usuário

--- src/app/core/services/auth.types.ts
+++ src/app/core/services/auth.types.ts
@@ -1,13 +1,22 @@
 export enum ModoAutenticacao {
     email,
     facebook,
-};
+}
+
+export enum TipoUsuario {
+    Cliente,
+    Admin,
+    Operador
+}
 
 export interface Usuario {
+    id: string;
     nome?: string;
-    email: string;
-    senha: string;
+    email?: string;
+    senha?: string;
+    tipo?: TipoUsuario;
 }
+
 export interface OpcoesAutenticacao {
     isCadastro: boolean;
     modoAutenticacao: ModoAutenticacao;
